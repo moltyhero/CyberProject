788ef0856151d5cf400a9627ae199019438b06e4
Matan <matanoded@gmail.com>
2017-11-02 14:33:26 +0200

    Maze basic generation

--- TheMaze/MainWindow.xaml.cs
+++ TheMaze/MainWindow.xaml.cs
@@ -23,6 +23,42 @@ namespace TheMaze
         public MainWindow()
         {
             InitializeComponent();
+            CreateMaze();
+        }
+
+        public void CreateMaze()
+        {
+            
+            int rows = 2;
+            int cols = 3;
+            // Get the maze size
+            /*System.Console.WriteLine("Enter row");
+            rows = Int32.Parse(Console.ReadLine());
+            System.Console.WriteLine("Enter col");
+            cols = Int32.Parse(Console.ReadLine());*/
+
+
+
+            // Add stack pannels to the stack pannels
+            for (int i = 0; i<cols; i++)
+            {
+                StackPanel stackPannel = new StackPanel();
+                stackPannel.Name = ("col" +i);
+
+                for (int j = 0; j < rows; j++)
+                {
+                    Rectangle rect = new Rectangle();
+                    rect.Height = 100;
+                    rect.Width = 100;
+                    rect.Fill = new SolidColorBrush(System.Windows.Media.Colors.Blue);
+                    stackPannel.Children.Add(rect);
+                }
+
+                mainStackPanel.Children.Add(stackPannel);
+            }
+
         }
     }
+    
+    
 }

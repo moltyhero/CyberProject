6640b7fbe5642b2b9567f9125d962b6f9d52c274
Matan Oded <matanoded@gmail.com>
2018-03-04 17:17:50 +0200

    Orginized the code a little bit

--- TheMaze/MainWindow.xaml.cs
+++ TheMaze/MainWindow.xaml.cs
@@ -20,55 +20,14 @@ namespace TheMaze
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
-            CreateMaze();
+            ScreenOrginizer screenOrginizer = new ScreenOrginizer(mazeWindow.Width, mazeWindow.Height);
+            screenOrginizer.CreateMaze(20,20, mainStackPanel);
         }
 
-        // Calculate the desired size of the rectangle
-        private double calcRectSize(double StackHeight, double StackWidth, int rows, int cols)
-        {
-            double HeightSize;
-            double WidthSize;
-            HeightSize = StackHeight / rows;
-            WidthSize = StackWidth / cols;
-            return Math.Min(HeightSize, WidthSize);
-        }
-
-        private void CreateMaze()
-        {
-            int rows = 20;
-            int cols = 22;
-            double w = mazeWindow.Width*4/6;
-            double h = mazeWindow.Height*4/6;
-            double size = calcRectSize(h, w, rows, cols);
-
-            // Add stack pannels to the main stack pannel
-            for (int i = 0; i<cols; i++)
-            {
-                StackPanel stackPannel = new StackPanel()
-                {
-                    Name = ("col" + i)
-                };
-
-                for (int j = 0; j < rows; j++)
-                {
-                    Rectangle rect = new Rectangle()
-                    {
-                        Height = size,
-                        Width = size,
-                        Fill = new SolidColorBrush(System.Windows.Media.Colors.Gray),
-                        StrokeThickness = 1,
-                        Stroke = Brushes.Violet
-                    };
-                    stackPannel.Children.Add(rect);
-                }
-
-                mainStackPanel.Children.Add(stackPannel);
-            }
-
-        }
     }
     
     
---
+++ TheMaze/ScreenOrginizer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TheMaze
+{
+    public class ScreenOrginizer
+    {
+        double screenHeight;
+        double screenWidth;
+
+        public ScreenOrginizer(double screenHeight, double screenWidth)
+        {
+            this.screenHeight = screenHeight;
+            this.screenWidth = screenWidth;
+        }
+
+        // Calculate the desired size of the rectangle
+        private double calcRectSize(double StackHeight, double StackWidth, int rows, int cols)
+        {
+            double HeightSize;
+            double WidthSize;
+            HeightSize = StackHeight / rows;
+            WidthSize = StackWidth / cols;
+            return Math.Min(HeightSize, WidthSize);
+        }
+
+        /// <summary>
+        /// Puts the maze on the screen, 
+        /// </summary>
+        /// <param name="rows"></param>
+        /// <param name="cols"></param>
+        public void CreateMaze(int rows, int cols, StackPanel main)
+        {
+            double w = screenWidth * 4 / 6;
+            double h = screenHeight * 4 / 6;
+            double size = calcRectSize(h, w, rows, cols);
+
+            // Add stack pannels to the main stack pannel
+            for (int i = 0; i < cols; i++)
+            {
+                StackPanel stackPannel = new StackPanel()
+                {
+                    Name = ("col" + i)
+                };
+
+                for (int j = 0; j < rows; j++)
+                {
+                    Rectangle rect = new Rectangle()
+                    {
+                        Height = size,
+                        Width = size,
+                        Fill = new SolidColorBrush(System.Windows.Media.Colors.Gray),
+                        StrokeThickness = 1,
+                        Stroke = Brushes.Violet
+                    };
+                    stackPannel.Children.Add(rect);
+                }
+
+                main.Children.Add(stackPannel);
+            }
+
+        }
+    }
+}
